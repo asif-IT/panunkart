2a746feeb16c033a4dcaf9dfd5680bbb66831004
asif-IT <asifnitsxr@gmail.com>
2015-09-08 18:01:52 +0530

    Update search_tab.php

--- includes/search_tab.php
+++ includes/search_tab.php
@@ -1,33 +1,48 @@
 <!-- ************************************ SEARCH TAB **************************************** -->
 
-<div id="search_tab" class="btn-group btn-group-justified" role="group">
-        <div class="btn-group" role="group">
-          <button type="button" class="btn btn-default" data-toggle="dropdown" >Property Type <span class="caret"></span></button>
-          <ul class="dropdown-menu">
-            <li><a href="">Test 1</a></li>
-            <li><a href="">Test 2</a></li>
-            <li><a href="">Test 3</a></li>
-          </ul>
-        </div>
-        <div class="btn-group" role="group">
-          <button type="button" class="btn btn-default" data-toggle="dropdown">Budget <span class="caret"></span></button>
-          <ul class="dropdown-menu">
-            <li><a href="">Test 1</a></li>
-            <li><a href="">Test 2</a></li>
-            <li><a href="">Test 3</a></li>
-          </ul>
-        </div>
-        <div class="btn-group" role="group">
-          <button type="button" class="btn btn-default" data-toggle="dropdown">Locality <span class="caret"></span></button>
-          <ul class="dropdown-menu">
-            <li><a href="">Test 1</a></li>
-            <li><a href="">Test 2</a></li>
-            <li><a href="">Test 3</a></li>
-          </ul>
-        </div>
-        <div class="btn-group" role="group">
-          <button type="button" class="btn btn-default">LOOK UP!</button>
-        </div>
-      </div>
+<div id="search_tab" class="container-fluid navbar navbar-inverse">
+	
+    <form>     
 
+	<div class="btn-group col-md-2">	
+	</div>	
+	
+        <!--Property Type button with dropdown menu-->
+        <div class="btn-group col-md-2">
+            <button type="button" data-toggle="dropdown" class="btn btn-primary dropdown-toggle navbar-btn">Property Type <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">Test 1</a></li>
+                <li><a href="#">Test 2</a></li>
+                <li><a href="#">Test 3</a></li>
+            </ul>
+        </div>
+		
+		<!--Budget button with dropdown menu-->
+        <div class="btn-group col-md-2">
+            <button type="button" data-toggle="dropdown" class="btn btn-primary dropdown-toggle navbar-btn">Budget <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">Test 1</a></li>
+                <li><a href="#">Test 2</a></li>
+                <li><a href="#">Test 3</a></li>
+            </ul>
+        </div>
+		
+		<!--Locality button with dropdown menu-->
+        <div class="btn-group col-md-2">
+            <button type="button" data-toggle="dropdown" class="btn btn-primary dropdown-toggle navbar-btn">Locality <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">Test 1</a></li>
+                <li><a href="#">Test 2</a></li>
+                <li><a href="#">Test 3</a></li>
+            </ul>
+        </div>
+		
+	<div class="btn-group col-md-2">
+		<button type="button" class="btn btn-success navbar-btn">LOOK UP!</button>
+	</div>
+	
+	<div class="btn-group col-md-2">	
+	</div>
+                
+    </form>
 </div>
